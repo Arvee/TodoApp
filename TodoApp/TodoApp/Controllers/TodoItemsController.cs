3998bfbecaf47e039cef2b90f8e27f997d51e763
Roopesh Vemoori <rvemoori@avidxchnage.com>
2018-03-19 00:23:40 -0400

    Todo App -Initial commit

---
+++ TodoApp/TodoApp/Controllers/TodoItemsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TodoApp.Models;
+using static System.Net.HttpStatusCode;
+
+namespace TodoApp.Controllers
+{
+    public class TodoItemsController : ApiController
+    {
+        public ITodoAppContext db = new TodoAppContext();
+        
+        public TodoItemsController() { }
+
+        public TodoItemsController(ITodoAppContext context)
+        {
+            db = context;
+        }
+        
+        // GET: api/TodoItems
+        [HttpGet]
+        [Route("api/TodoItems")]
+        public HttpResponseMessage GetTodoItems()
+        {
+            return Request.CreateResponse(GetTodoItemsList());
+        }
+
+        public IEnumerable<TodoItem> GetTodoItemsList()
+        {
+            return db.Items;
+        }
+
+        public async Task<IEnumerable<TodoItem>> GetAllTodoItemsAsync()
+        {
+            return await Task.FromResult(GetTodoItemsList());
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet]
+        [Route("api/TodoItems/{id}")]
+        [ResponseType(typeof(TodoItem))]
+        public HttpResponseMessage GetTodoItem(int id)
+        {
+            TodoItem todoItem = db.Items.Find(id);
+            if (todoItem == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+            return Request.CreateResponse(todoItem);
+        }
+
+        // PUT: api/TodoItems/5
+        [HttpPut]
+        [Route("api/TodoItems/{id}")]
+        [ResponseType(typeof(void))]
+        public HttpResponseMessage PutTodoItem(int id, TodoItem todoItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                //return BadRequest(ModelState);
+            }
+
+            if (id != todoItem.Id)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+            //db.Entry(todoItem).State = EntityState.Modified;
+            db.MarkAsModified(todoItem);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    //return NotFound();
+                    return new HttpResponseMessage(HttpStatusCode.NotFound);
+                }
+                else
+                {
+                    throw ;
+                }
+            }
+
+            //return StatusCode(NoContent);
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+
+        //PATCH: api/TodoItems/patchItem
+        [HttpPatch]
+        [Route("api/TodoItems/{patchItem}")]
+        public HttpResponseMessage PatchTodoItem(int itemId, TodoItemPatch patchItem)
+        {
+            if (patchItem == null || !ModelState.IsValid)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.Items.FirstOrDefault(i => i.Id == itemId);
+            if (todoItem == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+            todoItem.Title = patchItem.Title;
+            todoItem.Description = patchItem.Description;
+            todoItem.DueDate = patchItem.DueDate;
+
+            return Request.CreateResponse(todoItem);
+        }
+
+        // POST: api/TodoItems
+        [HttpPost]
+        [Route("api/TodoItems")]
+        [ResponseType(typeof(TodoItem))]
+        public HttpResponseMessage PostTodoItem([FromBody]TodoItem todoItem)
+        {
+            if (todoItem == null || !ModelState.IsValid)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            
+             db.Items.Add(todoItem);
+             db.SaveChanges();
+            var response = Request.CreateResponse(HttpStatusCode.Created, todoItem);
+            string uri = Url.Link("DefaultApi", new {id = todoItem.Id});
+            response.Headers.Location =  new Uri(uri);
+            return response;
+            //return CreatedAtRoute("DefaultApi", new { id = todoItem.Id }, todoItem);
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete]
+        [Route("api/TodoItems/{id}")]
+        [ResponseType(typeof(HttpResponseMessage))]
+        public HttpResponseMessage DeleteTodoItem(int id)
+        {
+            TodoItem todoItem = db.Items.Find(id);
+            if (todoItem == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Items.Remove(todoItem);
+            db.SaveChanges();
+
+            return Request.CreateResponse(todoItem);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TodoItemExists(int id)
+        {
+            return db.Items.Count(item  => item.Id == id) > 0;
+        }
+    }
+}
---
+++ TodoApp/TodoApp/Models/TodoAppContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TodoApp.Models;
+
+namespace TodoApp.Models
+{
+    public class TodoAppContext : DbContext, ITodoAppContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public TodoAppContext() : base("name=TodoAppContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<TodoApp.Models.TodoItem> Items { get; set; }
+        //public DbSet<TodoItem> Items { get; }
+        public void MarkAsModified(TodoItem item)
+        {
+            Entry(item).State = EntityState.Modified;
+        }
+    }
+}
